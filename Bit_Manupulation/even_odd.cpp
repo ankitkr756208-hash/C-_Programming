b8a9f8d57842b785d6e5a8ded379f368eaa344b5
FullstackDevelopment <ankitkr756208@gmail.com>
2026-03-29 11:30:23 +0530

    Linked List added code

--- Bit_Manupulation/even_odd.cpp
+++ Bit_Manupulation/even_odd.cpp
@@ -10,9 +10,27 @@ void oddEven(int num){
     }
 }
 
+int getIthBit(int num , int i){
+    int bitMask=1<<i;
+    if(!(num & bitMask)){
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+int setItBit(int num, int i){
+    int bitMask=1<<i;
+    return (num |bitMask);
+}
+
 int main(){
     oddEven(5);
     oddEven(51);
     oddEven(50);
+    cout<<getIthBit(6,2)<<endl;
+
+    cout<<setItBit(8,1)<<endl;
 
 }
