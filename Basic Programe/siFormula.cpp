f34b4bf8c7d57e1438bc6faa5868128aece8d5d6
FullstackDevelopment <ankitkr756208@gmail.com>
2026-06-04 11:25:15 +0530

    updated code

--- Basic Programe/siFormula.cpp
+++ Basic Programe/siFormula.cpp
@@ -2,9 +2,99 @@
 using namespace std;
 
 int main(){
-    int p,r,t,si;
-    cin>> p>> r>>t;
-    cout<<"Enter the value of p,r, and t"<<endl;
-    cout<<"si is :"<<(p*r*t)/100;
+//    int i=4,j=4;
+//    for(int i=0;i<4;i++){
+//     for(int j=0;j<4;j++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+
+//    }
+// second
+int n=5;
+for(int i=1;i<=n;i++ ){// right angle triangle
+    for(int j=1;j<=i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+for(int i=1;i<=n;i++ ){ // inverted right angle triangle
+    for(int j=1;j<=n-i+1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+for(int i=1;i<=n;i++){// diamond pattern
+    for(int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(int k=1;k<=2*i-1;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i-1;j++){
+        cout<<" ";
+    }
+    for(int k=1;k<=2*(n-i)+1;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if(i==1 || i==n || j==1 || j==n){
+            cout<<"*";
+        }
+        else {
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+// number traingle
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<j<<" ";
+    }
+    cout<<endl;
+}
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+//Character Triangle
+for(int i=1;i<=n;i++){
+    for(int j=0;j<i;j++){
+        cout<<char('A'+j)<<" ";
+    }
+    cout<<endl;
+}
+
+for(int i=1;i<=n;i++){
+    for(int j=0;j<i;j++){
+        cout<<char('A'+i)<<" ";
+    }
+    cout<<endl;
+}
+
+// floyds triangle
+int count=1;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<count<<" ";
+        count++;
+    }
+    cout<<endl;
+}
     
 }
